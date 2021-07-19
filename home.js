d4a3022d9ccb115642c0e1be3d1680bcac1863a8
othieno <nicothieno@gmail.com>
2021-07-19 03:03:48 +0300

    validated the form section

--- home.js
+++ home.js
@@ -19,29 +19,72 @@ if (i<10){
 return i;  
 }  
 /*========================================================================================*/
+var CC, YY, MM, DD, d, dayValue;
+var dayNames = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+var maleNames = ["Kwesi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
+var femaleNames = ["Akosua","Adjoa","Abena","Ekua","Aba","Efua","Amba"]
+
 var database=firebase.database();
 
 function checkforblank () {
-	if (document.getElementById('comment').value == "") {
-		alert('message cannot be empty');
-		document.getElementById('comment').style.borderColor = "red";
+	if (document.getElementById('comments').value == "") {
+		alert('String cannot be empty');
+		document.getElementById('comments').style.borderColor = "red";
 		return false;
 	}
 }
 
-function sendMessage() {
-  var email=document.getElementById("email").value;
-  var name=document.getElementById("name").value;
-  var message=document.getElementById("message").value;
+/*==========================================================================*/
 
-  var newMessageKey=database.ref().child('comments').push().key;
-  database.ref('comments/'+newMessageKey+'/Email').set(email);
-  database.ref('comments/'+newMessageKey+'/Name').set(name);
-  database.ref('comments/'+newMessageKey+'/message').set(message);
+function validate() {
+  var genders = document.getElementById('gender');
+  if(document.validForm.day.value == "") || document.validForm.day.value.length != 2 || document.validForm.day.value > 31 || {
+    alert("Please provide a valid birth date!");
+    document.validForm.day.focus();
+    return false;
+  }
+  else if(document.validForm.month.value == "") || isNaN(document.validForm.month.value) || document.validForm.month.length != 9 || document.validForm.month.value > 12 || {
+    alert("Please pick you month of birth!");
+    document.validForm.month.focus();
+    return false;
+  }
+  else if(document.validForm.year.value == "") || document.validForm.year.value > 12 || {
+    alert( "Please provide a valid Year of Birth" );
+    document.validForm.year.focus();
+    return false;
+  }
 }
 
 
-document.getElementById('contactForm').addEventListener('submit',submitForm)
+
+/*function validate() {
+  var genders = document.getElementById("gender");
+  if(document.validForm.day.value =="") || (document.validForm.day.value) ||
+  document.validForm.day.value.length != 2 {
+  alert( "Please provide a valid birth-date!" );
+  document.validForm.day.focus();
+  return false;
+  }
+  else if(document.validForm.month.value == "") || isNaN(document.validForm.month.value) ||
+  document.validForm.month.value.length != 9 || document.validForm.month.value > 12 || {
+  alert( "Please pick your month of birth!" );
+  return false;
+  }
+  else if(document.validForm.year.value == "") || document.validForm.year.value != 4 || {
+    alert( "Please provide a valid Year of Birth" );
+    document.validForm.year.focus() ;
+    return false;
+  }
+  else if(genders[0].checked==false && genders[1].checked==false) {
+    alert("You MUST select male or female");
+    return false;
+  }
+  else{
+    return true
+  }
+}*/
+
+document.getElementById('btn').addEventListener('submit',submitForm)
 
 function submitForm(e) {
   e.preventDefault();
@@ -50,7 +93,80 @@ function submitForm(e) {
 document.querySelector('.alert').getElementsByClassName.display='block';
 
 function hideAlert() {
-  document.querySelector('.alert').getElementsByClassName='none';
+  document.querySelector('.alert').getElementsByClassName='alert';
 }
 
 setTimeout(hideAlert,3000);
+/*=====================================================================================*/
+function checkButton() {
+  var getSelectedValue = document.querySelector('input[name="gender"]:checked');
+  var isvalid = checked < 0;
+
+  if(getSelectedValue != null) {
+    document.getElementById("disp").innerHTML
+    = getSelectedValue.value
+    + "Gender is selected"
+  }
+  else {
+    document.getElementById("error").innerHTML
+    = "*You have not selected any gender"
+  }
+  else {
+    document.getElementById("error1").innerHTML
+    = "*Only one gender"
+  }
+  switch(gender) {
+    case "male":
+      if (dayValue == 1) {
+        alert("Your Akan name is " + maleNames[0] + "!");
+      }
+      else if(dayValue == 2) {
+        alert("Your Akan name is " + maleNames[1] + "!");
+      }
+      else if(dayValue == 3) {
+        alert("Your Akan name is " + maleNames[2] + "!");
+      }
+      else if(dayValue == 4) {
+        alert("Your Akan name is " + maleNames[3] + "!");
+      }
+      else if(dayValue == 5) {
+        alert("Your Akan name is " + maleNames[4] + "!");
+      }
+      else if(dayValue == 6) {
+        alert("Your Akan name is " + maleNames[5] + "!");
+      }
+      else if(dayValue == -0) {
+        alert("Your Akan name is " + maleNames[6] + "!");
+      }
+    break;
+    case "female":
+      if (dayValue == 1) {
+        alert("Your Akan name is " + femaleNames[0] + "!");
+      }
+      else if(dayValue == 2) {
+        alert("Your Akan name is " + femaleNames[1] + "!");
+      }
+      else if(dayValue == 3) {
+        alert("Your Akan name is " + femaleNames[2] + "!");
+      }
+      else if(dayValue == 4) {
+        alert("Your Akan name is " + femaleNames[3] + "!");
+      }
+      else if(dayValue == 5) {
+        alert("Your Akan name is " + femaleNames[4] + "!");
+      }
+      else if(dayValue == 6) {
+        alert("Your Akan name is " + femaleNames[5] + "!");
+      }
+      else if(dayValue == -0) {
+        alert("Your Akan name is " + femaleNames[6] + "!");
+      }
+    break
+    default:
+  }
+}
+/*=====================================================================================*/
+function findName() {
+  dayValue = calculateDayValue();
+  getGender();
+}
